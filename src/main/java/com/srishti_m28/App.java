b9ec79b998fcc74c86fb8a4bcb9b3a3009e2fc47
SrishtiMalik28 <91114873+SrishtiMalik28@users.noreply.github.com>
2025-01-03 15:52:36 +0530

    first commit

---
+++ src/main/java/com/srishti_m28/App.java
@@ -0,0 +1,13 @@
+package com.srishti_m28;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
